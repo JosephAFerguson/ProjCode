8aa6379e2c009eb5da3707bbe39c1a7b8d3dc4fa
Joe Ferguson <fergujp@mail.uc.edu>
2025-09-17 14:07:28 -0400

    updates for building

--- UserInterfaceProj1/svelte.config.js
+++ UserInterfaceProj1/svelte.config.js
@@ -1,22 +1,5 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
-/** @type {import("@sveltejs/vite-plugin-svelte").SvelteConfig} */
 export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
   preprocess: vitePreprocess(),
 }
-
-import adapter from '@sveltejs/adapter-netlify';
-
-    /** @type {import('@sveltejs/kit').Config} */
-    export const config = {
-      kit: {
-        adapter: adapter({
-          // Optional: Enable Edge Functions
-          edge: false, 
-          // Optional: Split app into multiple functions for better performance
-          split: false 
-        })
-      }
-    };
