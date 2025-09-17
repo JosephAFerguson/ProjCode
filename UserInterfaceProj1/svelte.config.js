dfac60f3a20c3fb56c7ca9b5bda968ba4b1fc848
Joe Ferguson <fergujp@mail.uc.edu>
2025-09-17 13:29:08 -0400

    Added netlify adapter

--- UserInterfaceProj1/svelte.config.js
+++ UserInterfaceProj1/svelte.config.js
@@ -6,3 +6,17 @@ export default {
   // for more information about preprocessors
   preprocess: vitePreprocess(),
 }
+
+import adapter from '@sveltejs/adapter-netlify';
+
+    /** @type {import('@sveltejs/kit').Config} */
+    export const config = {
+      kit: {
+        adapter: adapter({
+          // Optional: Enable Edge Functions
+          edge: false, 
+          // Optional: Split app into multiple functions for better performance
+          split: false 
+        })
+      }
+    };
